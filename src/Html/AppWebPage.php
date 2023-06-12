9d477b6871f48c160587796475b26df9a37c9cd1
Nathan Eullaffroy <nathan.eullaffroy@etudiant.univ-reims.fr>
2023-06-12 17:32:09 +0200

    ajout de la classe AppWebPage.php

---
+++ src/Html/AppWebPage.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Html;
+
+class AppWebPage extends WebPage
+{
+    public function __construct(string $title = '')
+    {
+        parent::__construct($title);
+        $this->appendCssUrl("/css/style.css");
+    }
+
+    public function toHtml(): string
+    {
+        return <<<HTML
+    <!doctype html>
+    <html lang="fr">
+    <head>
+        <meta name ="viewport">
+        <meta charset="utf-8">
+        {$this->getHead()}
+        <title>{$this->getTitle()}</title>
+    </head>
+    <body>
+        <section class="header">
+            <h1>{$this->getTitle()}</h1>
+        </section>
+        <section class="content">
+            {$this->getBody()}
+        </section>
+            <p class="footer">{$this->getLastModification()}</p>
+    </body>
+    </html>
+HTML;
+    }
+}
