af64dfc4cb5a904f5da0924155f3dba31e560912
Nathan Eullaffroy <nathan.eullaffroy@etudiant.univ-reims.fr>
2023-06-12 22:41:49 +0200

    ajout d'index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo 'Hello Music!';
+declare(strict_types=1);
+
+use Entity\Collection\MovieCollection;
+use Html\AppWebPage;
+
+$webPage = new AppWebPage("Films");
+
+foreach(MovieCollection::findAll() as $movie) {
+    $webPage->appendContent(
+        <<<HTML
+        <a class="movie" href="/movie.php?movieId={$movie->getId()}">
+            <img class="movie__poster" src="poster.php?posterId={$movie->getPosterId()}">
+            <div class="movie__title">{$movie->getTitle()}</div> 
+        </a>\n
+        HTML
+    );
+}
+
+echo $webPage->toHtml();
