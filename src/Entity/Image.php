5da6e61a8c95fc8672bf4abcc65904e0349d6731
Thomas PAZZÉ <thomas.pazze@etudiant.univ-reims.fr>
2023-06-13 17:11:54 +0200

    fix: réparation de documentation pour Image.php

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -9,18 +9,11 @@ class Image
     protected int $id;
     protected string $jpeg;
 
-    /**
-     * Get the value of id
-     */
-    public function getId()
+    public function getId(): int
     {
         return $this->id;
     }
-
-    /**
-     * Get the value of jpeg
-     */
-    public function getJpeg()
+    public function getJpeg(): string
     {
         return $this->jpeg;
     }
