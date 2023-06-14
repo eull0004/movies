4b4d42df7ecb14672a364c648d403e663f2143c2
Thomas PAZZÉ <thomas.pazze@etudiant.univ-reims.fr>
2023-06-14 13:52:35 +0200

    feature: ajout du listage des film de l'acteur

--- public/actor.php
+++ public/actor.php
@@ -2,10 +2,11 @@
 
 declare(strict_types=1);
 
-use Entity\Cast;
+
 use Entity\People;
 use Entity\Exception\EntityNotFoundException;
 use Html\AppWebPage;
+use Entity\Collection\MovieCollection;
 
 if (!isset($_GET["peopleId"]) || !ctype_digit($_GET["peopleId"])) {
     header('Location: /movie.php', true, 302);
@@ -57,4 +58,26 @@ HTML
 
 /** boucle foreach sur les films d'un acteur */
 /** Afficher son role dans le film */
+$webPage->appendContent("<section class='actor__movies__info'>");
+
+foreach (MovieCollection::findAllByPeopleId($peopleId) as $movie) {
+    $cast = $actor->getCastByMovieId($movie->getId());
+    $webPage->appendContent(
+        <<<HTML
+		<a class="movie" href="/movie.php?movieId={$movie->getId()}">
+			<div class="actor__movie__info">
+				<div class="actor__movie__poster">
+					<img class="movie__poster" src="poster.php?posterId={$movie->getPosterId()}" alt="{$movie->getTitle()}">
+				</div>
+				<div class="actor__movie__title__and__date">
+					<div class="actor__movie__title">{$webPage->escapeString($movie->getTitle())}</div>
+					<div class="actor__movie__date">{$webPage->escapeString($movie->getReleaseDate())}</div>
+				</div>
+				<div class="actor__movie__role">{$webPage->escapeString($cast->getRole())}</div>
+			</div>
+		</a>\n
+	HTML
+    );
+}
+
 echo $webPage->toHtml();
